02e374c50738cc71919de71ff1935cfd471dbbf9
Steppe Schoolkate <S.Schoolkate@student.han.nl>
2022-08-16 12:44:02 +0200

    Added Challenge 3

---
+++ Challenge_3/main.py
@@ -0,0 +1,18 @@
+import random
+
+from google.cloud import bigquery
+
+if __name__ == '__main__':
+    project = 'data-engineer-tha'
+
+    client = bigquery.Client()
+    data_sizes = {}
+    for dataset_item in client.list_datasets(project=project):
+        size = 0
+        for table_item in client.list_tables(dataset_item.dataset_id):
+            table_ref = client.get_table(table_item)
+            size += table_ref.num_bytes
+        data_sizes[dataset_item.dataset_id] = size
+    sorted_by_size = dict(sorted(data_sizes.items(), key=lambda item: item[1], reverse=True))
+    for dataset_size in sorted_by_size:
+        print(dataset_size + ' = ' + str(sorted_by_size[dataset_size]) + ' bytes')

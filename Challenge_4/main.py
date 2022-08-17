4dda5b845e23dfbb28943a5a8268ebefd898816b
Steppe Schoolkate <S.Schoolkate@student.han.nl>
2022-08-17 12:48:21 +0200

    Added extra no column names feature

--- Challenge_4/main.py
+++ Challenge_4/main.py
@@ -3,13 +3,13 @@ import csv
 cleaned_keyword = 'cleaned_'
 
 
-def remove_headers(cvs_file_name):
+def remove_headers(cvs_file_name, has_column_names):
     with open(cvs_file_name, newline='') as csv_file:
         with open(cleaned_keyword + cvs_file_name, mode='w', newline='') as cleaned_csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             csv_writer = csv.writer(cleaned_csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for idx, row in enumerate(csv_reader):
-                if idx == 0:
+                if idx == 0 and has_column_names:
                     csv_writer.writerow(row)
                 elif not check_header(row):
                     csv_writer.writerow(row)
@@ -29,5 +29,6 @@ def check_header(row):
 
 
 if __name__ == '__main__':
-    remove_headers('sample_data_1_header_row.csv')
-    remove_headers('sample_data_3_header_rows.csv')
+    remove_headers('sample_data_1_header_row.csv', True)
+    remove_headers('sample_data_3_header_rows.csv', True)
+    remove_headers('sample_data_2_header_rows_no_column_names.csv', False)
